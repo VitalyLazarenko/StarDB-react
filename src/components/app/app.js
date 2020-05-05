2615e7c0d78a80dfddb72d8cabb159a272637d86
VitalyLazarenko <hristofor.xxxx@gmail.com>
2020-05-05 18:34:55 +0300

    Create people-page, refactoring structure project.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,33 +6,32 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import PeoplePage from "../people-page/people-page";
+import ErrorIndicator from "../error-indicator/error-indicator";
 
 export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        selectedPerson: null,
+        hasError: false,
     }
 
-    onPersonSelected = (id) => {
-        this.setState({
-            selectedPerson: id,
-        })
+    componentDidCatch() {
+        this.setState({hasError: true})
     }
+
     render() {
+        if(this.state.hasError){
+            return <ErrorIndicator/>
+        }
+
         return (
             <div>
-                <Header />
-                <RandomPlanet />
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={this.onPersonSelected}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>
+                <Header/>
+                <RandomPlanet/>
+                <PeoplePage/>
+                <PeoplePage/>
+                <PeoplePage/>
             </div>
         );
     }
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -16,7 +16,7 @@ export default class RandomPlanet extends Component {
 
     componentDidMount() {
         this.updatePlanet();
-        this.interval = setInterval(this.updatePlanet, 2500);
+        this.interval = setInterval(this.updatePlanet, 5000);
     }
 
     componentWillUnmount() {
